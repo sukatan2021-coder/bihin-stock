d9d938b187e26cb092ebc8d26facb758660dc311
sukatan2021-coder <sukatan2021@gmail.com>
2026-03-06 01:23:15 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "bihin-stock-v2";
+const CACHE_NAME = "bihin-stock-v3";
 const ASSETS = ["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png"];
 
 self.addEventListener("install", (event) => {
